b3db65b634ac79e2db5c3f4f6d7835b6dafe6fa0
rafaelsilvapdc <rafael.silva-ext@parceirodaconstrucao.com.br>
2023-10-09 15:52:12 -0300

    feature: reset password user

--- src/routes/user/user.route.ts
+++ src/routes/user/user.route.ts
@@ -9,11 +9,17 @@ export class UserRoute {
   
   public async init() {
     userRoute.post('/create', ValidatorUtil.validatorFieldsNotEmpty(['username', 'password'], 'body') ,this.createUser);
+    userRoute.put('/update/password', ValidatorUtil.validatorFieldsNotEmpty(['username', 'new_password'], 'body') ,this.updatePassword);
   }
 
   public async createUser(request: Request, response: Response) {
     const userAuthenticationService = new UserAuthenticationService();
     await userAuthenticationService.createUser(request, response);
+  }
+
+  public async updatePassword(request: Request, response: Response) {
+    const userAuthenticationService = new UserAuthenticationService();
+    await userAuthenticationService.updatePassword(request, response);
   }
 
   getRoute() {

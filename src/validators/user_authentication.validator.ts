64096ed3fa65b3f7891ad7fe60f3da9f3d1edfc0
Rafael Silva <rafaelhenriquesilva494@gmail.com>
2023-10-11 00:29:26 -0300

    feature: create user auth validator

---
+++ src/validators/user_authentication.validator.ts
@@ -0,0 +1,25 @@
+import { ValidationChain, validationResult } from 'express-validator';
+import { body, query, param } from 'express-validator';
+import { ValidatorUtil } from '../utils/validator.util';
+export class UserAuthenticationValidator {
+
+    static createUserAndLoginValidator() {
+        let usernameValidatorNotEmpty = body('username').notEmpty().withMessage('Username cannot be empty');
+        let passwordValidatorNotEmpty = body('password').notEmpty().withMessage('Password cannot be empty');
+        let usernameValidatorMin8 = body('username').isLength({ min: 8 }).withMessage('Username must have minimum length of 8 characters');
+        let passwordValidatorMin8 = body('password').isLength({ min: 8 }).withMessage('Password must have minimum length of 8 characters');
+
+        return ValidatorUtil.validateFields([usernameValidatorNotEmpty, passwordValidatorNotEmpty, usernameValidatorMin8, passwordValidatorMin8]);
+    }
+
+    static updatePasswordValidator() {
+        let newPasswordValidatorNotEmpty = body('new_password').notEmpty().withMessage('Password cannot be empty');
+        let newPasswordValidatorMin8 = body('new_password').isLength({ min: 8 }).withMessage('Password must have minimum length of 8 characters');
+        let usernameValidatorNotEmpty = body('username').notEmpty().withMessage('Username cannot be empty');
+        
+        return ValidatorUtil.validateFields([newPasswordValidatorNotEmpty, newPasswordValidatorMin8, usernameValidatorNotEmpty]);
+    }
+
+
+    
+}

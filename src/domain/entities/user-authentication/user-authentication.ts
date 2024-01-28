c79d0f25ff69ccf4b5a339e5fe993bd9f7749d14
Rafael Candido <rafaelhenriquesilva494@gmail.com>
2024-01-28 13:35:56 -0300

    DDD Infra Increment change and get properties by methods in User authentication Entity

--- src/domain/entities/user-authentication/user-authentication.ts
+++ src/domain/entities/user-authentication/user-authentication.ts
@@ -1,11 +1,11 @@
 export class UserAuthentication {
-    public id: string;
-    public username: string;
-    public token: string;
-    public password: string;
-    public createdAt: Date;
-    public updatedAt: Date;
-    public isActive: boolean;
+    private _id: string;
+    private _username: string;
+    private _token: string;
+    private _password: string;
+    private _createdAt: Date;
+    private _updatedAt: Date;
+    private _isActive: boolean;
   
     constructor({
       id,
@@ -16,13 +16,62 @@ export class UserAuthentication {
       updatedAt,
       isActive
     }: UserAuthenticationDTO) {
-      this.id = id;
-      this.username = username;
-      this.token = token;
-      this.password = password;
-      this.createdAt = createdAt;
-      this.updatedAt = updatedAt;
-      this.isActive = isActive;
+      this._id = id;
+      this._username = username;
+      this._token = token;
+      this._password = password;
+      this._createdAt = createdAt;
+      this._updatedAt = updatedAt;
+      this._isActive = isActive;
+    }
+
+    get id(): string {
+      return this._id
+    }
+
+    get username(): string {
+      return this._username
+    }
+
+    get token(): string {
+      return this._token
+    }
+
+    get password(): string {
+      return this._password
+    }
+
+    get createdAt(): Date {
+      return this._createdAt
+    }
+
+    get updatedAt(): Date {
+      return this._updatedAt
+    }
+
+    get isActive(): Boolean {
+      return this._isActive
+    }
+
+    changeUsername(username: string): void {
+      this._username = username
+    }
+
+    changeToken(token: string): void {
+      this._token = token
+    }
+
+    changePassword(password: string) : void {
+      this._password = password
+    }
+
+    changeActivity(isActive: boolean) : void {
+      this._isActive = isActive
+    }
+
+    changeUpdatedAt(updatedAt: Date) : void {
+      this._updatedAt = updatedAt
     }
   }
+  
   
